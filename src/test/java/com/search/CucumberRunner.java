f82ac00a0f7ccdd782dcec57dafe170ed22ce972
silvia.dimova <silvia.dimova@yahoo.com>
2020-01-16 12:15:21 +0000

    Setting CucumberRunner

---
+++ src/test/java/com/search/CucumberRunner.java
@@ -0,0 +1,18 @@
+package com.search;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = { "src/test/resources/features" },
+        glue = { "src/test/java/com/search/stepDefinitions" },
+        tags = { "@acceptance, @smoke" },
+        plugin = { "pretty", "html:target/cucumber-html-report", "json:target/cucumber-json-report.json" },
+        dryRun = true
+)
+
+public class CucumberRunner {
+    //unused
+}
---
+++ src/test/java/com/search/stepDefinitions/GoogleSearchStep.java
@@ -0,0 +1,4 @@
+package com.search.stepDefinitions;
+
+public class GoogleSearchStep {
+}
